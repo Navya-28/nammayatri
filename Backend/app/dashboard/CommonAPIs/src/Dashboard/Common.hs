f64947647056910dcdeefd978960b0a96c97c91b
Roman Bodavsky <roman.bodavskiy@metalamp.io>
2026-07-31 08:59:10 +0300

    backend/feat/msil: Admin Maker-Checker

--- Backend/app/dashboard/CommonAPIs/src/Dashboard/Common.hs
+++ Backend/app/dashboard/CommonAPIs/src/Dashboard/Common.hs
@@ -112,6 +112,8 @@ data PaymentOrder
 
 data PGPaymentSettlementReport
 
+data LedgerAdjustmentRequest
+
 data Role = DRIVER | FLEET
   deriving stock (Eq, Show, Generic)
   deriving anyclass (ToJSON, FromJSON, ToSchema, ToParamSchema)
--- Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_FinanceManagement.sql
+++ Backend/dev/migrations-read-only/provider-dashboard/Local_API_Management_FinanceManagement.sql
@@ -62,3 +62,18 @@ INSERT INTO atlas_dashboard.access_matrix (id, role_id, api_entity, user_access_
 
 -- {"api":"GetFinanceManagementFinanceSapJournalsTransactions","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
 INSERT INTO atlas_dashboard.access_matrix (id, role_id, api_entity, user_access_type, user_action_type) VALUES ( atlas_dashboard.uuid_generate_v4(), '37947162-3b5d-4ed6-bcac-08841be1534d', 'DSL', 'USER_FULL_ACCESS', 'PROVIDER_MANAGEMENT/FINANCE_MANAGEMENT/GET_FINANCE_MANAGEMENT_FINANCE_SAP_JOURNALS_TRANSACTIONS' ) ON CONFLICT DO NOTHING;
+
+
+------- SQL updates -------
+
+-- {"api":"PostFinanceManagementFinanceAdjustmentSubmit","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'finance.adjustment.write' ) ON CONFLICT DO NOTHING;
+
+-- {"api":"GetFinanceManagementFinanceAdjustmentList","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'finance.adjustment.read' ) ON CONFLICT DO NOTHING;
+
+-- {"api":"PostFinanceManagementFinanceAdjustmentApprove","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'finance.adjustment.write' ) ON CONFLICT DO NOTHING;
+
+-- {"api":"PostFinanceManagementFinanceAdjustmentReject","migration":"localAccessForRoleId","param":"37947162-3b5d-4ed6-bcac-08841be1534d","schema":"atlas_dashboard"}
+INSERT INTO atlas_dashboard.role_capability (role_id, capability_id) VALUES ( '37947162-3b5d-4ed6-bcac-08841be1534d', 'finance.adjustment.write' ) ON CONFLICT DO NOTHING;
